a6ee8c883b9c8d30bc93b9fefe3b023de274bd2e
rii <96197787+Rii1126@users.noreply.github.com>
2023-07-09 05:44:08 -0700

    delete

--- README.md
+++
@@ -1,5 +0,0 @@
-npx webpack --mode production
-
-cd dist && http-server
-
-npx http-server dist
--- src/index.js
+++
@@ -1,3 +0,0 @@
-console.log("start");
-const template = require('./templates/index.hbs');
-document.body.innerHTML = template({ title: 'Handlebars Webpack Example', message: 'Hello, Handlebars!' });
